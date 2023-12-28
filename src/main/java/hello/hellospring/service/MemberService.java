d27606a62912be5cd59383bff7ef5a450ffd6bf1
songyoungmin <domybest0908@gmail.com>
2023-12-29 00:35:22 +0900

    Section_7

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -19,7 +19,7 @@ public class MemberService {
 
     /** 회원 가입 **/
     public  Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
+            // 같은 이름이 있는 중복 회원 X
         validateDuplicateMember(member); //중복 검증
         memberRepository.save(member);
         return member.getId();
